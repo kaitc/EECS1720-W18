cd1eefe906c2fc9a9db37819f875942b141ec491
mb <mb@wijnendale-5>
2018-07-28 18:51:13 +0300

    ready for dist

---
+++ 1720_W18_Code/CM19_20/testClass.java
@@ -0,0 +1,12 @@
+package CM19_20;
+
+import java.io.PrintStream;
+
+public class testClass {
+
+	public static void main(String[] args) {
+		PrintStream out = System.out;
+		out.println("Hello world");
+	}
+
+}
